52726f67ca29d5c40d7e8d9a96917cbe2f8c6f5c
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-13 23:31:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,8 +14,11 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
+  i = 0
   birthday_kids.each do |kids_name, age|
-    
+    if i.length <= 12
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end

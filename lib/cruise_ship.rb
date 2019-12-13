9a65c975b6c268b5617ad0936a5a6965896233b0
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-13 23:32:27 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,28 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
+ 
+winner
+end
+
+
+
+# def age_appropriate_birthday(birthday_kids) 
+#   birthday_kids.each do |kids_name, age|
+    
+#   end
+  
+
+
+
+
+
+
+
+

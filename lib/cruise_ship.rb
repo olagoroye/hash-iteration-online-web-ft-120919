dc702779f74b3fa2c42bd919bf3fb8336a4577a4
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-13 23:13:30 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -22,3 +22,15 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids) 
+  bi
+  
+
+
+
+
+
+
+
+
